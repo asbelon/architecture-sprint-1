94faf4e37f0d61cb5c16af8d5199fd333d9921d1
Александр Белов <asbelon@gmail.com>
2024-09-07 16:24:38 +0300

    microfrontend

---
+++ frontend/microfrontend/host/src/App.jsx
@@ -0,0 +1,23 @@
+import React, { lazy }  from "react";
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+
+// const Card = lazy(() => import('places/Card').catch(() => {
+//  return { default: () => <div className='error'>Component is not available!</div> };
+// }));
+
+const App = () => (
+  <div className="container">
+    <div>Name: host</div>
+    <div>Framework: react</div>
+    <div>Language: JavaScript</div>
+    <div>CSS: Empty CSS</div>
+  </div>
+);
+const rootElement = document.getElementById("app")
+if (!rootElement) throw new Error("Failed to find the root element")
+
+const root = ReactDOM.createRoot(rootElement)
+
+root.render(<App />)
